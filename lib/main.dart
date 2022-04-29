eeb63eee51ceaa8ca6a786054782efd20c2c0b35
SamadhiPerera <samadhiwathsala96@gmail.com>
2022-04-29 18:57:44 +0530

    Set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:auth_app/routes.dart';
 import 'package:auth_app/screans/auth/get_phone.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
